c6c919b72d9fe199e49b9ca0b032cb97519a255e
ansar <ansarsaudagar40@gmail.com>
2024-10-20 11:57:12 +0530

    authentication basic setup done with sanctum

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Http\Middleware\HandleCors::class, // <-- This handles CORS
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/product-categories', [ProductCategoryController::class, 'index']);
 Route::post('/product', [ProductCategoryController::class,'store']);
+
+// Auth routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
