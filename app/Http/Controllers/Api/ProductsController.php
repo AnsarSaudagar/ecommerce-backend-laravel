94afb563fbbd3aec182a674f02054fcaba202879
ansar <ansarsaudagar40@gmail.com>
2024-10-22 09:38:27 +0530

    Added routes for product

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -9,6 +9,20 @@ use Illuminate\Http\Request;
 class ProductsController extends Controller
 {
     public function index(){
-        $products = Products::orderBy("id","desc")->paginate(10);
+        // $products = Products::orderBy("id","desc")->paginate(10);
+        $allProducts = Products::all();
+        return response()->json($allProducts);
     }
+
+    public function product($id){
+        $product = Products::find($id);
+        return response()->json($product);
+    }
+
+    public function productsByCategory($category_id){
+        $products = Products::where("category_id",$category_id)->get(); 
+        return response()->json($products);
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,20 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductCategoryController;
+use App\Http\Controllers\Api\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/product-categories', [ProductCategoryController::class, 'index']);
-Route::post('/product', [ProductCategoryController::class,'store']);
+Route::post('/add-category', [ProductCategoryController::class,'store']);
 
 // Auth routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Products Route
+Route::get('/all-products', [ProductsController::class,'index']);
+Route::get('/product/{id}', [ProductsController::class,'product']);
+Route::get('/products/{category_id}', [ProductsController::class,'productsByCategory']);
+
+
